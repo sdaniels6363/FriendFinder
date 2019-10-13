0b46611972a0fcc752a5de228a6d99b7508fb703
Scott Daniels <sdaniels6363@gmail.com>
2019-10-13 11:33:49 -0400

    html route testing successful

---
+++ server.js
@@ -0,0 +1,22 @@
+// dependecies
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// route paths
+var htmlRoutes = (path.join(__dirname),"./app/routing/htmlRoutes.js")
+
+// routing
+console.log(htmlRoutes);
+
+require(htmlRoutes)(app);
+// require("./app/routing/apiRoutes")(app); // not ready yet
+
+app.listen(PORT, function(){
+  console.log(`App listening on http://localhost:${PORT}`)
+})
