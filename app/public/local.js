4429b61e156a24f007c595a39efcb6ff5fc8c9e1
Scott Daniels <sdaniels6363@gmail.com>
2019-10-13 17:47:52 -0400

    client-side js sends successful post to api

--- app/public/local.js
+++ app/public/local.js
@@ -1,10 +1,33 @@
+function getScores(answers){
+  var questions = $(".chosen-select")
+  for (i=0;i<questions.length;i++){
+    var question = questions[i].id;
+    var score = $(`#${question}`).val();
+    answers.push(score);
+  }
+  return answers;
+}
+
+
 $(document).ready(function() {
 
 
   $("#submit").on("click",function(event){
     event.preventDefault();
 
-    console.log(event)
+    var answers = []; // declare empty array for scores
+
+    // build the JSON body to be sent in the post request.
+    var userForm = {
+      "name": $("#name").val(),
+      "photo": $("#imgLink").val(),
+      "score": getScores(answers)
+    };
+    console.log(userForm);
+
+    $.post("/api/friends", userForm).then(function(){
+      console.log("New User added")
+    });
 
 
   });
