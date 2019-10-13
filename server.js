d4c9f1ac10ba8ee105a58ce40dd2257a497b1033
Scott Daniels <sdaniels6363@gmail.com>
2019-10-13 16:11:23 -0400

    served public dir so css and client.js could load

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(express.static(path.join(__dirname,"./app/public"))); 
 
 // route paths
 var apiRoutes = (path.join(__dirname, "./app/routing/apiRoutes.js"));
