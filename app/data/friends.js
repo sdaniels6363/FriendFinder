aa2a85106315d2a0d0af9f78189d873caa102267
Scott Daniels <sdaniels6363@gmail.com>
2019-10-13 20:51:57 -0400

    finished pre populating the friends data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,46 @@
-var friends = [{
-  name: "John Doe",
-  photo: "N/A",
-  score: [3,6,8]
-}];
+var friends = [
+  {
+    name: "Chandler",
+    photo: "https://www.cheatsheet.com/wp-content/uploads/2018/09/Chandler-on-Friends.png",
+    score: [ 1, 5, 5, 3, 1, 1, 4, 4, 1, 2 ]
+  },{
+    name: "Monica",
+    photo: "https://static0.srcdn.com/wordpress/wp-content/uploads/2019/07/Courtney-Cox-as-Monica-in-Friends.jpg",
+    score: [ 5, 2, 5, 1, 4, 1, 5, 4, 5, 2 ]
+  },{
+    name: "Ross",
+    photo: "https://img.cinemablend.com/filter:scale/cb/0/e/7/3/2/3/0e73232b91f36632a93b8260633e612fcec40da30727388919d849fbf986852a.jpg?mw=600",
+    score: [ 2, 1, 4, 4, 4, 5, 3, 5, 4, 1 ]
+  },{
+    name: "Rachel",
+    photo: "https://img.maximummedia.ie/her_ie/eyJkYXRhIjoie1widXJsXCI6XCJodHRwOlxcXC9cXFwvbWVkaWEtaGVyLm1heGltdW1tZWRpYS5pZS5zMy5hbWF6b25hd3MuY29tXFxcL3dwLWNvbnRlbnRcXFwvdXBsb2Fkc1xcXC8yMDE3XFxcLzA4XFxcLzI0MTMwMTM1XFxcL1NjcmVlbi1TaG90LTIwMTctMDgtMjQtYXQtMTMuMDAuNTktMTAyNHg1ODkucG5nXCIsXCJ3aWR0aFwiOjY0MCxcImhlaWdodFwiOjM2MCxcImRlZmF1bHRcIjpcImh0dHBzOlxcXC9cXFwvd3d3Lmhlci5pZVxcXC9hc3NldHNcXFwvaW1hZ2VzXFxcL2hlclxcXC9uby1pbWFnZS5wbmc_aWQ9YjZmODRkNjI3YjQxMTRmMDBmNTBcIixcIm9wdGlvbnNcIjpbXX0iLCJoYXNoIjoiNTJiMTVkMDA0ODhlYmFhYWVlNGRkYmUyZmIzNDE1ZGU3YzFmYTRkZSJ9/screen-shot-2017-08-24-at-13-00-59-1024x589.png",
+    score: [ 1, 5, 3, 2, 3, 1, 2, 5, 1, 3 ]
+  },{
+    name: "Phoebe",
+    photo: "https://imgix.bustle.com/rehost/2016/9/13/c9e38185-9873-4dd9-82eb-6d873280415e.png?w=970&h=546&fit=crop&crop=faces&auto=format&q=70",
+    score: [ 5, 5, 5, 3, 2, 1, 1, 4, 1, 1 ]
+  },{
+    name: "Joey",
+    photo: "https://img.maximummedia.ie/joe_ie/eyJkYXRhIjoie1widXJsXCI6XCJodHRwOlxcXC9cXFwvbWVkaWEtam9lLm1heGltdW1tZWRpYS5pZS5zMy5hbWF6b25hd3MuY29tXFxcL3dwLWNvbnRlbnRcXFwvdXBsb2Fkc1xcXC8yMDE1XFxcLzEwXFxcLzI5MTExMzEyXFxcL0pvZXkuanBnXCIsXCJ3aWR0aFwiOjc2NyxcImhlaWdodFwiOjQzMSxcImRlZmF1bHRcIjpcImh0dHBzOlxcXC9cXFwvd3d3LmpvZS5pZVxcXC9hc3NldHNcXFwvaW1hZ2VzXFxcL2pvZVxcXC9uby1pbWFnZS5wbmc_aWQ9MjY0YTJkYmUzNzBmMmM2NzVmY2RcIixcIm9wdGlvbnNcIjpbXX0iLCJoYXNoIjoiMDY2ODU4NGY3MDNjZjg1MWJiOWU1YTgxMDJlNWE4ZTUxMzBlNGI1MiJ9/joey.jpg",
+    score: [ 5, 4, 4, 5, 5, 3, 3, 4, 2, 2 ]
+  },{
+    name: "Gunther",
+    photo: "https://images.immediate.co.uk/production/volatile/sites/3/2019/09/friends-gunther-8fbd4c8.jpg?quality=90&resize=620,413",
+    score: [ 1, 5, 2, 3, 5, 3, 2, 3, 5, 3 ]
+  },{
+    name: "Janice",
+    photo: "https://mtv.mtvnimages.com/uri/mgid:ao:image:mtv.com:221550?quality=0.8&format=jpg&width=1440&height=810&.jpg",
+    score: [ 5, 3, 4, 2, 3, 5, 4, 2, 5, 4 ]
+  },{
+    name: "Richard",
+    photo: "https://assets-jpcust.jwpsrv.com/thumbnails/r0ieeobb-720.jpg",
+    score: [ 1, 4, 4, 1, 3, 2, 3, 2, 3, 1 ]
+  },{
+    name: "Eddie",
+    photo: "https://img.cinemablend.com/filter:scale/cb/f/0/2/9/4/1/f029419bd954e98b459855850b9b989ed6c002609a91835a782d7a0fd1dbeb83.jpg?mw=600",
+    score: [ 5, 1, 5, 1, 1, 1, 3, 4, 1, 5 ]
+  }
+
+];
 
 module.exports = friends;
